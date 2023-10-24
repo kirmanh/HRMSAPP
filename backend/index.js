eec99c7e0061055516ba71f8cc7da0a79f4a9c71
kirman <hakeemkirman@gmail.com>
2023-10-24 18:38:39 +0500

    this is commit 5

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = { credentials: true, origin: process.env.URL || "*" };
+const corsOptions = {
+  credentials: true,
+  origin: process.env.URL || "../public/index.html",
+};
 
 app.use(cors(corsOptions));
 app.use(json());
 app.use(cookieParser());
 
-app.use("/", express.static(join(__dirname, "public")));
+app.get("/", function (req, res) {
+  res.sendfile(__dirname + "./public/index.html");
+});
+
+// app.use("/", express.static(join(__dirname, "./public")));
 
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
